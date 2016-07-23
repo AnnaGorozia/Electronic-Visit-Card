a1b92fb943eab23b2ae75729d1b8cbdc9034deaf
AnnaGorozia <ana.gorozia@gmail.com>
2016-07-24 00:09:51 +0400

    User registration done

--- EVC/app/src/main/java/com/evc/tasks/ServerUserRegisterTask.java
+++ EVC/app/src/main/java/com/evc/tasks/ServerUserRegisterTask.java
@@ -1,28 +1,31 @@
 package com.evc.tasks;
+
 import com.evc.models.User;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import org.json.JSONObject;
-
-import java.io.InputStream;
-import java.util.ArrayList;
-
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class ServerUserRegisterTask extends UserRegisterTask {
+
     private static Gson gson = new GsonBuilder().create();
-    private String url = "http://192.168.76.236:8082/UserService";
+    private String url = "http://192.168.43.188:8082/UserService";
 
     private String message;
+
     @Override
-    protected String doInBackground(String... params){
-        Client client = ClientBuilder.newClient();
+    protected String doInBackground(String... params) {
+
         url += "/registrateUser";
+
         User user = new User();
         user.setFirstName(params[0]);
         user.setLastName(params[1]);
@@ -32,14 +35,50 @@ public class ServerUserRegisterTask extends UserRegisterTask {
 
         String body = gson.toJson(user);
 
-        Response simulatorResponse = client.target(url)
-                .request(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-                .post(Entity.entity(body, MediaType.APPLICATION_JSON));
-
-
-        this.message = simulatorResponse.toString();
+        String message = sendPostRequest(url, body);
 
         return message;
+    }
+
+    private String sendPostRequest(String url, String body) {
+        HttpURLConnection httpURLConnection;
+        String data = body;
+        String result = null;
+        try{
+            //Connect
+            httpURLConnection = (HttpURLConnection) ((new URL(url).openConnection()));
+            httpURLConnection.setDoOutput(true);
+            httpURLConnection.setRequestProperty("Content-Type", "application/json");
+            httpURLConnection.setRequestProperty("Accept", "application/json");
+            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.connect();
+
+            //Write
+            OutputStream os = httpURLConnection.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+            writer.write(data);
+            writer.close();
+            os.close();
+
+            //Read
+            BufferedReader br = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream(),"UTF-8"));
+
+            String line = null;
+            StringBuilder sb = new StringBuilder();
+
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+            br.close();
+            result = sb.toString();
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
