4e39643078aa554bde3511c662457e9f45f1e938
khrak <mamuka_sakhelashvili@yahoo.com>
2016-07-25 23:07:26 +0400

    removing duplicate fields from profile pages

--- app/src/main/java/com/evc/ProfileActivity.java
+++ app/src/main/java/com/evc/ProfileActivity.java
@@ -11,7 +11,7 @@ public class ProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.update_info);
+        setContentView(R.layout.company_profile);
 
         //drawProfilePage();
 
