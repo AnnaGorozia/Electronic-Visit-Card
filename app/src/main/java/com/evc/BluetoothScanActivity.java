54ac0c54f7cbb8c243523876dbeafabd69946271
khrak <mamuka_sakhelashvili@yahoo.com>
2016-07-26 17:00:01 +0400

    add bluetooth scan stuff

--- app/src/main/java/com/evc/BluetoothScanActivity.java
+++ app/src/main/java/com/evc/BluetoothScanActivity.java
@@ -1,7 +1,20 @@
 package com.evc;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by khrak on 7/26/16.
  */
-public class BluetoothScanActivity {
+public class BluetoothScanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bluetooth_scan);
+
+    }
+
+
+
 }
